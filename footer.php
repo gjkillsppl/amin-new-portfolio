953ccf81683fc533970ce096e097e1b6eba837c0
gjkillsppl <amin@belerofon.com.mx>
2025-03-20 10:03:45 -0600

    added php for reusable

---
+++ footer.php
@@ -0,0 +1,16 @@
+    <footer>
+        <p>2024 © Amin Wejebe Shanahan</p>
+        <div class="social-wrap">
+            <ul>
+                <li><a href="#" class="cursor-link">in</a></li>
+                <!-- Add more social links as needed -->
+            </ul>
+        </div>
+    </footer>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+</html>
